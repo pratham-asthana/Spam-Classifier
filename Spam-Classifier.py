29e45ce0b95f15a455590f22136a97bee0a28c98
Pratham Asthana <83860600+pratham-asthana@users.noreply.github.com>
2024-07-05 18:19:46 +0530

    Add files via upload
    Here are the project files, along with the screenshots and screen recording of the functional project.

---
+++ Spam-Classifier.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+nltk.download('punkt')
+nltk.download('stopwords')
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    y = []
+    for i in text:
+        if i.isalnum():
+            if i not in stopwords.words('english') and i not in string.punctuation:
+                y.append(ps.stem(i))
+    return " ".join(y)
+
+
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.title("Email/SMS Spam Classifier")
+input_sms = st.text_area("Enter the message")
+if st.button('Predict'):
+    try:
+        transformed_sms = transform_text(input_sms)
+        vector_input = tfidf.transform([transformed_sms])
+        result = model.predict(vector_input)[0]
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
